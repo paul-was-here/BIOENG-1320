1dafe13dc1879fd9c9d7f11d95ea3cc585ef9f3e
paul-was-here <paul.g.kullmann@gmail.com>
2025-01-09 18:52:28 -0500

    Add files via upload

---
+++ Project 1/Tutorial_Part1.m
@@ -0,0 +1,38 @@
+% Part 1
+t = linspace(-10,10,20/0.001);
+x3 = zeros(1,20/0.001);
+x30 = zeros(1,20/0.001);
+x300 = zeros(1,20/0.001);
+
+for n = 1:3
+    x3 = x3 + (-1)^n / (pi * n) * sin(0.4*pi*t*n);
+end
+
+for n = 1:30
+    x30 = x30 + (-1)^n / (pi * n) * sin(0.4*pi*t*n);
+end
+
+for n = 1:300
+    x300 = x300 + (-1)^n / (pi * n) * sin(0.4*pi*t*n);
+end
+
+figure
+subplot(3,1,1)
+plot(t,x3)
+title('3 Terms')
+xlabel('t')
+ylabel('x(t)')
+
+subplot(3,1,2)
+plot(t,x30)
+title('30 Terms')
+xlabel('t')
+ylabel('x(t)')
+
+subplot(3,1,3)
+plot(t,x300)
+title('300 Terms')
+xlabel('t')
+ylabel('x(t)')
+
+% x(t) is a sawtooth wave
