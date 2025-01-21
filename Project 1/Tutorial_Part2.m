d052e5ef8a413b20ada34c5e7fcadc75d9723aaa
paul-was-here <paul.g.kullmann@gmail.com>
2025-01-20 20:49:37 -0500

    Add files via upload

--- Project 1/Tutorial_Part2.m
+++ Project 1/Tutorial_Part2.m
@@ -1,12 +1,22 @@
+%Paul Kullmann & Waleed Sabri
+
 N = 300; %choose number of summations here
 
+%Variable definitions
 times = -10:0.001:10;
 freqs = (1:N)*(0.4*pi);
 amplitudes = ones(N).*(-1).^(1:N) ./ (pi.*(1:N));
 
-x = amplitudes * sin(transpose(freqs)*times);
+%Function call
+x = fourierSeries(amplitudes, freqs, times);
 
+%Plotting
 plot(times,x);
 title(strcat(num2str(N), " Sums"));
 xlabel('t');
 ylabel('x(t)');
+
+%Function defenition
+function [x] = fourierSeries(a, w, t)
+    x = a * sin(transpose(w)*t);
+end
